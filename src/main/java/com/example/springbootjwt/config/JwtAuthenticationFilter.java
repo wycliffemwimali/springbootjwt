e906193b59042cd0b251ca0c1e81912c99e65ace
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 12:01:56 +0300

    auth token

--- src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -51,6 +52,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         userDetails,
                         null,
                         userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
                 );
             }
         }
