940b09bd61d69d973dc946cbec642f05491386f4
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 06:43:43 +0300

    check jwt token

--- src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
@@ -20,6 +20,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
           @NonNull  FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
 
     }
 }
