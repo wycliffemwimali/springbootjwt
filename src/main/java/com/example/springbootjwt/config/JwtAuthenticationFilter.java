0abf986071c09a6c5f8fbb4bf3cc4f5433bedbeb
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 12:07:32 +0300

    update auth token

--- src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
@@ -57,6 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 authToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
 
