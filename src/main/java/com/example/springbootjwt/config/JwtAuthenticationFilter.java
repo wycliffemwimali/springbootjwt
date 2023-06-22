1ce7ba92a595a9125be466cd9e0481fa0b7c556e
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 06:29:07 +0300

    method implementation

--- src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springbootjwt/config/JwtAuthenticationFilter.java
@@ -4,13 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+          @NonNull HttpServletRequest request,
+          @NonNull  HttpServletResponse response,
+          @NonNull  FilterChain filterChain
+    ) throws ServletException, IOException {
 
     }
 }
