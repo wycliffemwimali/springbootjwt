ce7bd12f2345fe4c2292f0a029dd8b03d2009afc
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 05:43:28 +0300

    primary key

--- src/main/java/com/example/springbootjwt/user/User.java
+++ src/main/java/com/example/springbootjwt/user/User.java
@@ -1,6 +1,8 @@
 package com.example.springbootjwt.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +16,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "_user")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstname;
     private String secondname;
