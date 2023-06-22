638a6c33d796d7f56bdecf55abe5a8f332e0413b
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 05:34:43 +0300

    user info

--- src/main/java/com/example/springbootjwt/user/User.java
+++ src/main/java/com/example/springbootjwt/user/User.java
@@ -1,2 +1,9 @@
-package com.example.springbootjwt.user;public class User {
+package com.example.springbootjwt.user;
+
+public class User {
+    private Integer id;
+    private String firstname;
+    private String secondname;
+    private String email;
+    private String password;
 }
