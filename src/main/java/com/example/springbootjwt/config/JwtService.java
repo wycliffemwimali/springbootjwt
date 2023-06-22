6aaf47344cd3905c9d503e857aea9ed0011ad3c2
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 11:21:19 +0300

    validate token

--- src/main/java/com/example/springbootjwt/config/JwtService.java
+++ src/main/java/com/example/springbootjwt/config/JwtService.java
@@ -47,6 +47,10 @@ public class JwtService {
         return (username.equals(userDetails.getUsername() && !isTokenExpired(token)));
     }
 
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token);
+    }
+
     //extract single claim
     public <T> T extactClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims = extractAllClaims(token);
