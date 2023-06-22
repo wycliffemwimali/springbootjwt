e54dd55a472e9b5aa947b617cfe6183113e3e807
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 06:17:00 +0300

    User Repo

--- src/main/java/com/example/springbootjwt/user/UserRepository.java
+++ src/main/java/com/example/springbootjwt/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.example.springbootjwt.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
