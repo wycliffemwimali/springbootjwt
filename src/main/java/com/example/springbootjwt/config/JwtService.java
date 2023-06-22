cab1e9773df248d17d1868eabf5d92fd89da55e2
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 10:16:18 +0300

    extract all claims method

--- src/main/java/com/example/springbootjwt/config/JwtService.java
+++ src/main/java/com/example/springbootjwt/config/JwtService.java
@@ -1,10 +1,22 @@
 package com.example.springbootjwt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String extractUsername(String token) {
         return null;
+    }
+
+    //class to extract all claims
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
