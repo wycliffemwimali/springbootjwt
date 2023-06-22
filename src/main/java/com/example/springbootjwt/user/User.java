2c70826ba53c016d36db1ae183ce16e1f67260f9
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-22 05:36:42 +0300

    user class

--- src/main/java/com/example/springbootjwt/user/User.java
+++ src/main/java/com/example/springbootjwt/user/User.java
@@ -1,5 +1,14 @@
 package com.example.springbootjwt.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Integer id;
     private String firstname;
